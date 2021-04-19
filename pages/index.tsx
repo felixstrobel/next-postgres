4ac53ea0d42416214f9695614f3f6e77fa42226b
Felix Strobel <main.felixstrobel@gmail.com>
2021-04-19 22:33:43 +0200

    Working example with database (postgres) support.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 import Head from 'next/head'
+import prisma from "../lib/prisma";
+import {GetStaticProps} from "next";
 
-export default function Home() {
-  return (
-    <div className="w-100 h-screen grid place-items-center">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <p>
-        Hello World
-      </p>
-    </div>
-  )
+export const getStaticProps: GetStaticProps = async () => {
+    const feed = await prisma.user.findFirst({
+        where: {
+            name: "Felix"
+        }
+    })
+    return {props: {feed}}
+}
+
+export default function Home(props) {
+    return (
+        <div className="w-100 h-screen grid place-items-center">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <div>
+                Hello {props.feed.name}
+            </div>
+        </div>
+    )
 }
